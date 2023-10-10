5c367f3c593c9c6cf7157274786fe861d3fa5f84
RobinPICODEV <131764066+RobinPICODEV@users.noreply.github.com>
2023-10-10 16:29:49 +0200

    UPDATE add get routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const dessertController = require('./src/MenuRequests/dessertController.js');
+const entreeController = require('./src/MenuRequests/entreeController.js');
+
+const customController = require('./src/MenuRequests/customController.js');
+
+
+const API_PORT = 3000;
+
+app.get('/dessert', dessertController.getDessertData);
+app.get('/entree', entreeController.getEntreeData);
+app.get('/plat', platController.getPlatData);
+
+app.get('/custom/base', customController.getBaseCustomData);
+app.get('/custom/ingredient', customController.getIngredientCustomData);
+
+app.listen(API_PORT, () => {
+    console.log('Server listening on port:', API_PORT);
+});
