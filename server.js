a58324bb43a59d2cb4ce7e3290cac9244f2f06a3
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-10-24 11:15:30 +0200

    add deleteUser, createcmd

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const customController = require('./src/MenuRequests/customController.js');
 //post
 const loginController = require('./src/UserRequests/loginController.js');
 const { postCreateUser } = require('./src/UserRequests/createUserController.js');
+const { postCreateCommand } = require('./src/CommandRequests/createCommandController.js');
+
+//delete
+const { deleteUser } = require('./src/UserRequests/deleteController.js');
 
 const API_PORT = 3000;
 
@@ -25,6 +29,10 @@ app.get('/custom/ingredient', customController.getIngredientCustomData);
 // post
 app.post('/create/user', postCreateUser)
 app.post('/login', loginController.postLogin)
+app.post('/create/command', postCreateCommand)
+
+//delete
+app.delete('/delete/user', deleteUser)
 
 app.listen(API_PORT, () => {
     console.log('Server listening on port:', API_PORT);
