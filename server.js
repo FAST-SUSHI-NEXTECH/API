9f2a8426222c17465dc2a11b957e7ef7aa8e6edd
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-10-24 11:04:32 +0200

    Update login,create user

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
 
+//get
 const dessertController = require('./src/MenuRequests/dessertController.js');
 const entreeController = require('./src/MenuRequests/entreeController.js');
 const platController = require('./src/MenuRequests/platController.js')
 
 const customController = require('./src/MenuRequests/customController.js');
 
+//post
+const loginController = require('./src/UserRequests/loginController.js');
+const { postCreateUser } = require('./src/UserRequests/createUserController.js');
 
 const API_PORT = 3000;
 
+// get
 app.get('/dessert', dessertController.getDessertData);
 app.get('/entree', entreeController.getEntreeData);
 app.get('/plat', platController.getPlatData);
 
 app.get('/custom/base', customController.getBaseCustomData);
 app.get('/custom/ingredient', customController.getIngredientCustomData);
+
+// post
+app.post('/create/user', postCreateUser)
+app.post('/login', loginController.postLogin)
 
 app.listen(API_PORT, () => {
     console.log('Server listening on port:', API_PORT);
