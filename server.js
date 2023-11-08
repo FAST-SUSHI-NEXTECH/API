e7428a83e2faff3491408a70054412dbb060a11f
Talace <0x.88ta.ko@gmail.com>
2023-11-08 18:33:15 +0100

    create route orderState2 & createOrder

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const { getOrder } = require('./src/OrderRequests/orderController.js');
 //post
 const loginController = require('./src/UserRequests/loginController.js');
 const { postCreateUser } = require('./src/UserRequests/createUserController.js');
-const { postCreateCommand } = require('./src/CommandRequests/createCommandController.js');
+const { postCreateOrder } = require('./src/OrderRequests/createOrderController.js');
+
+// put (update)
+const { putStateOrder2 } = require('../API/src/OrderRequests/stateOrderController.js');
 
 //delete
 const { deleteUser } = require('./src/UserRequests/deleteController.js');
@@ -35,7 +38,10 @@ app.get('/order', getOrder)
 // post
 app.post('/create/user', postCreateUser)
 app.post('/login', loginController.postLogin)
-app.post('/create/command', postCreateCommand)
+app.post('/create/order', postCreateOrder)
+
+// put (UPDATE)
+app.put('/order/state/update/2', putStateOrder2)
 
 //delete
 app.delete('/delete/user', deleteUser)
