a2b867d25c1bcfdc0b59ecce3c92d484a93cd37e
Talace <0x.88ta.ko@gmail.com>
2023-11-08 20:18:26 +0100

    CREATE - getClient

---
+++ src/AdminRequests/getClientController.js
@@ -0,0 +1,15 @@
+const pool = require('../dbConnection'); 
+
+const getClient = async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const query = await connection.query("SELECT * FROM user WHERE perm = '0'");
+        connection.release();
+        res.json(query);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
+
+module.exports = { getClient };
