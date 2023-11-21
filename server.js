1213eaa6c478bd28bde553c4d1de5e9c64ff1861
StopThatTalace <0x.88ta.ko@gmail.com>
2023-11-21 15:52:45 +0100

    ADD allow cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 //get
 const dessertController = require('./src/MenuRequests/dessertController.js');
