f79e50d910e463921d547f915624402816fe8335
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:51:01 +0100

    add - documentation for GetAllOrder

--- src/OrderRequests/orderController.js
+++ src/OrderRequests/orderController.js
@@ -1,5 +1,35 @@
 const pool = require('../dbConnection'); 
 
+
+/**
+ * @swagger
+ * /order:
+ *   get:
+ *     summary: Get all orders
+ *     description: Returns a list of all customer orders
+ *     tags:
+ *       - Order
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: 
+ *               - id_order: 1,
+ *                 id_client: 123,
+ *                 order_state: 1,
+ *                 date: '2023-01-01T12:00:00Z'
+ *               - id_order: 2,
+ *                 id_client: 456,
+ *                 order_state: 1,
+ *                 date: '2023-01-02T12:00:00Z'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { message: 'Internal server error.' }
+ */
+
 const getOrder = async (req, res) => {
     try {
         const connection = await pool.getConnection();
