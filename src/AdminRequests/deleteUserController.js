b27c2c4b3a603b8f6c99cbb7e0c897ec6e62ef9f
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:49:18 +0100

    add - documentation for deleteUser

--- src/AdminRequests/deleteUserController.js
+++ src/AdminRequests/deleteUserController.js
@@ -1,5 +1,43 @@
 const pool = require('../dbConnection'); 
 
+/**
+ * @swagger
+ * /user/delete:
+ *   delete:
+ *     summary: Delete a user
+ *     description: Deletes a user based on the provided username
+ *     tags:
+ *       - Admin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The username of the user to be deleted
+ *             required:
+ *               - username
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'User deleted successfully.' }
+ *       401:
+ *         description: Delete failed, user not found
+ *         content:
+ *           application/json:
+ *             example: { message: 'Delete failed. User not found.' }
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { message: 'Internal server error.' }
+ */
+
 const deleteUser = async (req, res) => {
     try {
         const connection = await pool.getConnection();
