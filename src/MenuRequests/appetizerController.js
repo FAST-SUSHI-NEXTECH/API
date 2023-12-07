d1d00f827676c0ce429bb397851dd787c8eaa65e
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:49:56 +0100

    add - documentation for GetAppetizer

--- src/MenuRequests/appetizerController.js
+++ src/MenuRequests/appetizerController.js
@@ -1,5 +1,29 @@
 const pool = require('../dbConnection'); 
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Product
+ *     description: Request for product
+ */
+
+/**
+ * @swagger
+ * /appetizer:
+ *   get:
+ *     summary: Get base data
+ *     description: Returns base data
+ *     tags:
+ *       - Product
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: { dessert: 'Chocolate Cake', price: 10.99 }
+ */
+
+
 const getAppetizerData = async (req, res) => {
     try {
         const connection = await pool.getConnection();
