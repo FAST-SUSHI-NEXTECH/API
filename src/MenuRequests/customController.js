4c22d65fd1683b72024d3001afaaac6aae1cb1f5
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:55:08 +0100

    update - sql request for ingredient

--- src/MenuRequests/customController.js
+++ src/MenuRequests/customController.js
@@ -44,7 +44,7 @@ const getBaseCustomData = async (req, res) => {
 const getIngredientCustomData = async (req, res) => {
     try {
         const connection = await pool.getConnection();
-        const users = await connection.query('SELECT * FROM ingredient');
+        const users = await connection.query('SELECT * FROM product WHERE type_product="ingredient"');
         connection.release();
         res.json(users);
     } catch (error) {
