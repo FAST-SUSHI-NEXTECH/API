22c1e9c15861dd7da57a7afab3e893461ab46ead
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 14:21:22 +0100

    UPDATE sql request createUser

--- src/UserRequests/createUserController.js
+++ src/UserRequests/createUserController.js
@@ -6,13 +6,13 @@ const postCreateUser = async (req, res) => {
         const {
             last_name,
             first_name,
+            username,
+            password,
             email,
-            tel,
-            identifiant,
-            mdp
+            tel
         } = req.body;
 
-        if (!last_name || !first_name || !email || !tel || !identifiant || !mdp) {
+        if (!last_name || !first_name || !username || !password || !email || !tel) {
             res.status(400).json({ message: 'All fields are required.' });
             return;
         }
@@ -20,7 +20,7 @@ const postCreateUser = async (req, res) => {
         const fieldNames = Object.keys(req.body);
 
         const placeholders = fieldNames.map(() => '?').join(', ');
-        const query = `INSERT INTO user (${fieldNames.join(', ')}, perm) VALUES (${placeholders}, 0)`;
+        const query = `INSERT INTO user (${fieldNames.join(', ')}, perm) VALUES (${placeholders}, 1)`;
         const values = fieldNames.map(fieldName => req.body[fieldName]);
 
         const result = await connection.query(query, values);
