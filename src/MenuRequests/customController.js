84029ed1bf36d87bbc14d9fb4fbc3bd7a242167d
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:54:39 +0100

    add - documentation for custom base & and ingredient

--- src/MenuRequests/customController.js
+++ src/MenuRequests/customController.js
@@ -1,5 +1,33 @@
 const pool = require('../dbConnection'); 
 
+/**
+ * @swagger
+ * /base:
+ *   get:
+ *     summary: Get base data
+ *     description: Returns base data
+ *     tags:
+ *       - Product
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: { dessert: 'Chocolate Cake', price: 10.99 }
+ * /ingredient:
+ *   get:
+ *     summary: Get ingredient data
+ *     description: Returns ingredient data
+ *     tags:
+ *       - Product
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: { ingredient: 'Cheese', price: 2.99 }
+ */
+
 const getBaseCustomData = async (req, res) => {
     try {
         const connection = await pool.getConnection();
