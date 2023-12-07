391def2b5133dd7ce874af8c751faf5e2b92828d
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-07 15:51:35 +0100

    add - documentation for PostUserLogin

--- src/UserRequests/loginController.js
+++ src/UserRequests/loginController.js
@@ -1,5 +1,45 @@
 const pool = require('../dbConnection'); 
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticates a user based on the provided username and password
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: User authenticated successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'User authenticated successfully', user: { id: 1, username: 'john_doe', perm: 1 } }
+ *       401:
+ *         description: Authentication failed, invalid username or password
+ *         content:
+ *           application/json:
+ *             example: { message: 'Authentication failed. Invalid username or password.' }
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { message: 'Internal server error.' }
+ */
+
 const postLogin = async (req, res) => {
     try {
         const connection = await pool.getConnection();
