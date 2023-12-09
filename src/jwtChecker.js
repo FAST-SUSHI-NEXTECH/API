c229a8068accade0524256b39f96f822a65c07a0
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-09 11:47:23 +0100

    fix - jwt checker: get Bearer string when checking

--- src/jwtChecker.js
+++ src/jwtChecker.js
@@ -1,21 +1,35 @@
 const jwt = require('jsonwebtoken');
-const { secretKey } = require('./config');
-
+const { secretKey, secretUsername, secretRole } = require('./config');
 
 function verifyToken(req, res, next) {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
 
-    if (!token) {
+    if (!authHeader) {
         return res.status(401).json({ message: 'Unauthorized: Token missing' });
     }
 
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    // Check if the header has the Bearer prefix
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer !== 'Bearer' || !token) {
+        return res.status(401).json({ message: 'Unauthorized: Invalid token format' });
     }
 
-    req.user = decoded;
-    next();
-    });
+    try {
+        // Verify if the JWT format is correct and if it's encoded with the correct secret key
+        const decoded = jwt.verify(token, secretKey);
+
+        // Check if the decoded payload has the expected username and role
+        if (decoded && decoded.username === secretUsername && decoded.role === secretRole) {
+            req.user = decoded;
+            next();
+        } else {
+            return res.status(401).json({ message: 'Unauthorized: Invalid username or role' });
+        }
+    } catch (err) {
+        console.error('Error verifying token:', err);
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
 }
+
 module.exports = { verifyToken };
