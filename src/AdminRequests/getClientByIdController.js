b931019384d04e304b5b3a8a9cc9d36fe6904621
StopThatTalace <0x.88ta.ko@gmail.com>
2023-12-20 09:06:20 +0100

    add - getClientById func & commentary

---
+++ src/AdminRequests/getClientByIdController.js
@@ -0,0 +1,61 @@
+const pool = require('../dbConnection'); 
+
+
+/**
+ * @swagger
+ * /user/client/id:
+ *   post:
+ *     summary: Get client details by user ID
+ *     description: Returns details of a client based on the provided user ID.
+ *     tags:
+ *       - Admin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id_user:
+ *                 type: integer
+ *                 description: The ID of the user (client) to retrieve.
+ *             required:
+ *               - id_user
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: 
+ *               - id_user: 39
+ *                 last_name: "Doe"
+ *                 first_name: "John"
+ *                 username: "toto"
+ *                 password: "string"
+ *                 email: "john.doe@example.com"
+ *                 tel: "1234567890"
+ *                 permission: 1
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { message: 'Internal server error.' }
+ */
+
+const postClientById = async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const { id_user } = req.body;        
+
+        const query = 'SELECT * FROM user WHERE id_user = ? AND permission = 1';
+
+        const result = await connection.query(query,[id_user]);
+        connection.release();
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
+
+module.exports = { postClientById };
