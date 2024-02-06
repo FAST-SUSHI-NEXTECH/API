61745c55d0a5a923d4e310c8dbe74223caea7e02
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-06 18:01:43 +0100

    create - count total user with full doc

---
+++ src/AdminRequests/getCountUserController.js
@@ -0,0 +1,46 @@
+/**
+ * @swagger
+ * /user/count:
+ *   get:
+ *     summary: Get all clients
+ *     description: Returns a list of all clients with permission level 1
+ *     tags:
+ *       - Admin
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:  
+ *               - total_user: 47
+
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { message: 'Internal server error.' }
+ */
+
+const pool = require('../dbConnection'); 
+
+
+const getUserCount = async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const result = await connection.query("SELECT COUNT(*) AS total_user FROM user");
+        connection.release();
+        
+        const serializedResult = JSON.parse(JSON.stringify(result, (key, value) =>
+            typeof value === 'bigint' ? Number(value) : value
+        ));
+
+        res.status(200).json(serializedResult);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
+
+module.exports = { getUserCount };
