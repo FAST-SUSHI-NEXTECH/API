0a9ab55853e4410682442b49091bdb7d7c62737b
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-20 09:56:36 +0100

    create - update product with full doc, handle wrong base

--- src/AdminRequests/putIngredientController.js
+++ src/AdminRequests/putIngredientController.js
@@ -1,21 +1,79 @@
+/**
+ * @swagger
+ * /product/update:
+ *   put:
+ *     summary: Update a product
+ *     description: Update product data
+ *     tags:
+ *       - Admin
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id_product:
+ *                 type: integer
+ *               type_product:
+ *                 type: string
+ *               product_name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       '200':
+ *         description: Product update successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Update successful
+ *       '401':
+ *         description: Update failed
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Update failed, wrong product_id or values
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Internal server error.
+ */
+
 const pool = require('../dbConnection');
 
 
-const putIngredient = async (req, res) => {
+const putProduct = async (req, res) => {
     try {
         const connection = await pool.getConnection();
-        const { product_name, description, price, quantity, id_product } = req.body;
 
-        // Use a parameterized query to prevent SQL injection
-        const query = "UPDATE product SET product_name = ?, description = ?, price = ?, quantity = ? WHERE id_product = ? AND id_product BETWEEN 101 AND 199";
-        const command = await connection.execute(query, [product_name, description, price, quantity, id_product]);
+        console.log("body", req.body);
 
-        connection.release();
+        const { type_product, product_name, description, price, quantity, id_product } = req.body;
 
-        if (command.affectedRows === 0) {
-            res.status(401).json({ message: 'Update failed, wrong ingredient_id or values' });
+        if (type_product === 'base' || type_product === 'dessert' || type_product === 'appetizer' || type_product === 'plate') {
+            // Use a parameterized query to prevent SQL injection
+            const query = "UPDATE product SET type_product = ?, product_name = ?, description = ?, price = ?, quantity = ? WHERE id_product = ?";
+            const command = await connection.execute(query, [type_product, product_name, description, price, quantity, id_product]);
+
+            console.log(command);
+            connection.release();
+
+            if (command.affectedRows === 0) {
+                res.status(401).json({ message: 'Update failed, wrong product_id or values' });
+            } else {
+                res.json({ message: 'Update successful' });
+            }
         } else {
-            res.json({ message: 'Update successful' });
+            res.status(400).json({ message: 'Update failed, wrong product_type (only: base, dessert, appetizer, plate)' });
         }
     } catch (error) {
         console.error(error); // Log the error for debugging
@@ -23,4 +81,4 @@ const putIngredient = async (req, res) => {
     }
 };
 
-module.exports = { putIngredient };
+module.exports = { putProduct };
